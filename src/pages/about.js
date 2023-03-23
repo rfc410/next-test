920129ef2dea70a83e3ad4e5156ce74d6fabfe42
Caio Rodrigues <rfcaio90@gmail.com>
2023-03-22 22:54:34 -0300

    start project

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const AboutPage = () => (
+  <>
+    <h1>That was pathetic!</h1>
+
+    <Link href="/">Go to home page</Link>
+  </>
+)
+
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const HomePage = () => (
+  <>
+    <h1>You will never win!</h1>
+
+    <Link href="/about">Go to about page</Link>
+  </>
+)
+
+export default HomePage
